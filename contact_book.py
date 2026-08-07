773a8fa671ce63671f85453a00300d3f6d62bf30
Tariful <tarifulhoque99@gmail.com>
2026-08-08 00:17:39 +0530

    Implement contact book with json storage

--- contact_book.py
+++ contact_book.py
@@ -1,59 +1,65 @@
-
+import json
 contacts = []
+with open("contacts.json", "r") as file:
+    contacts = json.load(file)
+
 while True:
-    print("\n==== CONTACT BOOK====")
+    print("=== CONTACT BOOK ===")
+    if len(contacts) == 0:
+        print("No Contacts found")
+    else:
+        for i, contact in enumerate(contacts, start=1):
+            print(f"{i}. {contact['name']} - {contact['phone']}")
+
     print("1. Add Contact")
-    print("2. View Contact")
-    print("3. Search Contact")
-    print("4. Delete Contact")
-    print("5. Exit")
-
+    print("2. Search Contact")
+    print("3. Delete Contact")
+    print("4. Exit")
+   
     try:
-
-     choice = int(input("Choose an option: "))
+            choice = int(input("Choose an option: "))
     except ValueError:
-      print("The choice input should be integer!")
-      break
-
+             print("The choice input should be integer!")
+             continue
+   
     if choice == 1:
-        name = input("Enter the name: ")
-        phone = input("Enter phone number: ")
-
-        contact = {
+       name = input("Enter the name: ")
+       phone = input("Enter phone number: ")
+   
+       contact = {
           "name": name,
           "phone":  phone
-        }
-        print(contact)
-        contacts.append(contact)
-        print("Contact added successfully!")
+          }
+       contacts.append(contact)
+       print("Contact added successfully!")
+       with open("contacts.json", "w") as file:
+            json.dump(contacts, file, indent=4)
+
     if choice == 2:
-       if len(contacts) == 0:
-          print("No contacts found.")
-       else:
-          for contact in contacts:
-             print(contact)
-    if choice == 3:
        search_name = input("Enter name to search: ")
        found = False
-       for contact in contacts:
-          if contact["name"] == search_name:
-             print(contact["name"], "-", contact["phone"])
-             found = True
-             if not found: 
-                print("Contact not found.")
-    if choice == 4:
-       delete_name = input("Enter name to delete: ")
-       found = False
-       for contact in contacts:
-          if contact["name"] == delete_name:
-             contacts.remove(contact)
-             print("Contact deleted")
-             found = True
-             break
-          if not found:
-             print("Contact not found")
-    if choice == 5:
-       print("See ya! ")
-       break
-       
+    for contact in contacts:
+         if contact["name"].lower() == search_name.lower():
+            print(contact["name"], "-", contact["phone"])
+            found = True
+            break
+    if not found: 
+             print("Contact not found.")
 
+    if choice == 3:
+     delete_name = input("Enter name to delete: ")
+     found = False
+    for contact in contacts:
+        if contact["name"].lower() == delete_name.lower():
+            contacts.remove(contact)
+            with open("contacts.json", "w") as file:
+                 json.dump(contacts, file, indent=4)
+            print("Contact deleted")
+            found = True
+            break
+    if not found:
+            print("Contact not found")
+
+    if choice == 4:
+            print("Goodbye! ")
+            break
